87f0594c01c54f013d7ca107f83e06073e97aa61
Fernandin <fpatric25@gmail.com>
2025-01-10 00:14:44 -0300

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './authInterceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor])
+    ),
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideClientHydration(),
+  ]
 };
